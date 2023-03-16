6f2128a6a8038ab3ae6c13a0485533f7ccefd95f
JJUNIWEB <csj53055@naver.com>
2023-03-16 20:09:19 +0900

    백준 3일차

---
+++ baekjoonEX/src/baekjoonEX2/Test2587.java
@@ -0,0 +1,24 @@
+package baekjoonEX2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Test2587 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int arr [] = new int [5];
+		int sum = 0;
+		
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+	}
+
+}
