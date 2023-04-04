07f0829faa00990c87ac5499c32abf1606e49d4f
JJUNIWEB <csj53055@naver.com>
2023-04-04 19:10:26 +0900

    스프링 postgresql 연동 진행중

---
+++ TeamProject/src/main/java/com/earth/withdang/UserDao.java
@@ -0,0 +1,19 @@
+package com.earth.withdang;
+
+import java.util.List;
+
+public interface UserDao {
+
+	void save(User user);
+
+    void update(User user);
+
+    void delete(User user);
+
+    User findById(int id);
+
+    List<User> findAll();
+}
+
+
+
